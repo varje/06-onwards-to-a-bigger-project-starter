a6cb6ec813792fba135568f4c1da70a53733b525
varje <varjekivila@gmail.com>
2025-10-03 16:55:30 +0300

    add detail page design and content

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Paide_ordulinnuse_varemed.JPG/1920px-Paide_ordulinnuse_varemed.JPG"
+      title="A First meetup"
+      address="Some street, some city"
+      description="The meetup description"
+    />
+  );
+}
+
+export default MeetupDetails;
