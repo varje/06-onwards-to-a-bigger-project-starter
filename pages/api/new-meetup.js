63e42b7534a1e45a58f564adcea4e1c6f8499652
varje <varjekivila@gmail.com>
2025-10-10 14:31:24 +0300

    add mongodb connection to seperate file

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -3,9 +3,7 @@ import { MongoClient } from 'mongodb';
 async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
-    const client = await MongoClient.connect(
-      'secret'
-    );
+    const client = await connectToDatabase();
     const db = client.db();
 
     const meetupsCollection = db.collection('meetups');
